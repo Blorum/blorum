c00a95a2652198510163b61ef8836ffff3f6b332
SorenEricMent <sorenericment@gmail.com>
2022-12-16 22:47:45 +0800

    sync workflow

--- blorum.sql
+++ blorum.sql
@@ -1,10 +1,5 @@
-CREATE DATABASE if not exists blorum;
-use blorum;
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,8 +17,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `aid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
+  `aid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
   `title` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `excerpt` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -33,7 +28,7 @@ CREATE TABLE `articles` (
   `history` json NOT NULL,
   `statistics` json NOT NULL,
   `slug` char(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `articles`
@@ -49,19 +44,20 @@ INSERT INTO `articles` (`aid`, `uid`, `title`, `content`, `excerpt`, `tags`, `ca
 --
 
 CREATE TABLE `categories` (
-  `cid` int(10) UNSIGNED NOT NULL,
+  `cid` int UNSIGNED NOT NULL,
+  `type` tinyint NOT NULL,
   `name` varchar(64) NOT NULL,
   `parent` text NOT NULL,
   `children` text NOT NULL,
   `statistics` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`cid`, `name`, `parent`, `children`, `statistics`) VALUES
-(1, '_blorum_root', '', '', '{}');
+INSERT INTO `categories` (`cid`, `type`, `name`, `parent`, `children`, `statistics`) VALUES
+(1, 0, '_blorum_root', '', '', '{}');
 
 -- --------------------------------------------------------
 
@@ -70,18 +66,18 @@ INSERT INTO `categories` (`cid`, `name`, `parent`, `children`, `statistics`) VAL
 --
 
 CREATE TABLE `comments_a` (
-  `cid` int(10) UNSIGNED NOT NULL,
-  `aid` int(10) UNSIGNED NOT NULL,
-  `nid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
-  `depth` tinyint(4) NOT NULL,
+  `cid` int UNSIGNED NOT NULL,
+  `aid` int UNSIGNED NOT NULL,
+  `nid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `depth` tinyint NOT NULL,
   `content` json NOT NULL,
-  `reply_to` int(10) UNSIGNED NOT NULL,
+  `reply_to` int UNSIGNED NOT NULL,
   `children` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `type` varchar(16) NOT NULL,
   `statistics` json NOT NULL,
   `history` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comments_a`
@@ -97,18 +93,18 @@ INSERT INTO `comments_a` (`cid`, `aid`, `nid`, `uid`, `depth`, `content`, `reply
 --
 
 CREATE TABLE `comments_p` (
-  `cid` int(10) UNSIGNED NOT NULL,
-  `pid` int(10) UNSIGNED NOT NULL,
-  `nid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
-  `depth` tinyint(4) UNSIGNED NOT NULL,
+  `cid` int UNSIGNED NOT NULL,
+  `pid` int UNSIGNED NOT NULL,
+  `nid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `depth` tinyint UNSIGNED NOT NULL,
   `content` json NOT NULL,
-  `reply_to` int(10) UNSIGNED NOT NULL,
+  `reply_to` int UNSIGNED NOT NULL,
   `children` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `type` tinyint(4) NOT NULL,
+  `type` tinyint NOT NULL,
   `statistics` json NOT NULL,
   `history` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comments_p`
@@ -124,15 +120,15 @@ INSERT INTO `comments_p` (`cid`, `pid`, `nid`, `uid`, `depth`, `content`, `reply
 --
 
 CREATE TABLE `comments_u` (
-  `cid` int(10) UNSIGNED NOT NULL,
-  `to_uid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
-  `reply_to` int(10) UNSIGNED NOT NULL,
+  `cid` int UNSIGNED NOT NULL,
+  `to_uid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `reply_to` int UNSIGNED NOT NULL,
   `content` json NOT NULL,
-  `type` tinyint(4) NOT NULL,
+  `type` tinyint NOT NULL,
   `statistics` json NOT NULL,
   `history` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comments_u`
@@ -150,7 +146,39 @@ INSERT INTO `comments_u` (`cid`, `to_uid`, `uid`, `reply_to`, `content`, `type`,
 CREATE TABLE `config` (
   `flag` varchar(64) NOT NULL,
   `value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`flag`, `value`) VALUES
+('allowed_comment_to_article', 'true'),
+('allowed_comment_to_post', 'true'),
+('allowed_comment_to_user', 'true'),
+('allowed_login', 'true'),
+('allowed_register', 'true'),
+('default_avatar', '/statics/avatar.png'),
+('default_email_protocol', 'smtp'),
+('enable_email_register_challenge', 'false'),
+('ip_detect_header', 'X-Forwarded-For'),
+('ip_detect_method', 'connection'),
+('ip_rate_limit_bypass_whitelist', '[127.0.0.1]'),
+('ip_rate_limit_create', '{\r\n	\"article\": 12,\r\n	\"posts\": 12,\r\n	\"notes\": 30,\r\n	\"react\": 64,	\r\n	\"comment\": 60\r\n}'),
+('ip_rate_limit_edit', '\r\n{\r\n\r\n	\"article\": 12,\r\n	\"posts\": 20,\r\n	\"notes\": 30,\r\n	\"react\": 128,	\r\n	\"comment\": 60\r\n	\r\n}'),
+('ip_rate_limit_login', '30'),
+('ip_rate_limit_remove', '\r\n{\r\n\r\n	\"article\": 12,\r\n	\"posts\": 20,\r\n	\"notes\": 30,\r\n	\"react\": 128,	\r\n	\"comment\": 60\r\n	\r\n}'),
+('lazy_process', '0'),
+('register_default_role', 'user'),
+('removed_res_keep', 'true'),
+('removed_res_keep_time', '604800'),
+('sendmail_config', '{}'),
+('ses_config', '{}'),
+('site_excerpt', 'This is a Blorum site, where you could publish blogs and chat.'),
+('site_logo', '/favicon.ico'),
+('site_title', 'Yet another Blorum site.'),
+('site_url', '127.0.0.1'),
+('smtp_config', '{}');
 
 -- --------------------------------------------------------
 
@@ -159,15 +187,15 @@ CREATE TABLE `config` (
 --
 
 CREATE TABLE `favorite_set` (
-  `fid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
+  `fid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
   `name` varchar(64) NOT NULL,
   `description` text NOT NULL,
   `content` json NOT NULL,
   `is_private` tinyint(1) NOT NULL,
   `subscribers` longtext NOT NULL,
   `statistics` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -176,15 +204,16 @@ CREATE TABLE `favorite_set` (
 --
 
 CREATE TABLE `forum` (
-  `fid` int(10) UNSIGNED NOT NULL,
+  `fid` int UNSIGNED NOT NULL,
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `excerpt` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `default_permission` json NOT NULL,
   `attach` json NOT NULL,
+  `root_categories` text NOT NULL,
   `pin` json NOT NULL,
   `statistics` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -193,12 +222,12 @@ CREATE TABLE `forum` (
 --
 
 CREATE TABLE `logs` (
-  `lid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
-  `level` tinyint(4) NOT NULL DEFAULT '1',
+  `lid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `level` tinyint NOT NULL DEFAULT '1',
   `content` json NOT NULL,
-  `timestamp` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `timestamp` bigint UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -207,10 +236,11 @@ CREATE TABLE `logs` (
 --
 
 CREATE TABLE `messages` (
-  `uid` int(10) UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `viewed` tinyint(1) NOT NULL,
   `content` json NOT NULL,
-  `timestamp` bigint(20) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `timestamp` bigint UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -219,15 +249,15 @@ CREATE TABLE `messages` (
 --
 
 CREATE TABLE `notes` (
-  `nid` int(10) UNSIGNED NOT NULL,
-  `aid` int(10) UNSIGNED NOT NULL,
-  `cid` int(10) UNSIGNED NOT NULL,
-  `from_pos` bigint(20) UNSIGNED NOT NULL,
-  `to_pos` bigint(20) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
+  `nid` int UNSIGNED NOT NULL,
+  `aid` int UNSIGNED NOT NULL,
+  `cid` int UNSIGNED NOT NULL,
+  `from_pos` bigint UNSIGNED NOT NULL,
+  `to_pos` bigint UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
   `content` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `history` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `notes`
@@ -243,10 +273,10 @@ INSERT INTO `notes` (`nid`, `aid`, `cid`, `from_pos`, `to_pos`, `uid`, `content`
 --
 
 CREATE TABLE `posts` (
-  `pid` int(10) UNSIGNED NOT NULL,
-  `aid` int(10) UNSIGNED NOT NULL,
-  `fid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
+  `pid` int UNSIGNED NOT NULL,
+  `aid` int UNSIGNED NOT NULL,
+  `fid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
   `title` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `is_conversation` tinyint(1) NOT NULL DEFAULT '0',
@@ -254,7 +284,7 @@ CREATE TABLE `posts` (
   `statistics` json NOT NULL,
   `history` json NOT NULL,
   `status` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `posts`
@@ -270,12 +300,12 @@ INSERT INTO `posts` (`pid`, `aid`, `fid`, `uid`, `title`, `content`, `is_convers
 --
 
 CREATE TABLE `reports` (
-  `rid` int(10) UNSIGNED NOT NULL,
-  `uid` int(10) UNSIGNED NOT NULL,
-  `reason` text NOT NULL,
+  `rid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `reason` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `type` varchar(16) NOT NULL,
-  `tid` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `tid` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -286,10 +316,101 @@ CREATE TABLE `reports` (
 CREATE TABLE `roles` (
   `name` varchar(64) NOT NULL,
   `type` tinyint(1) NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `with_rate_limit` tinyint(1) NOT NULL,
   `permissions` json NOT NULL,
   `rate_limits` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rv_articles`
+--
+
+CREATE TABLE `rv_articles` (
+  `aid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `title` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `excerpt` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `tags` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `category` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `history` json NOT NULL,
+  `slug` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rv_comments_a`
+--
+
+CREATE TABLE `rv_comments_a` (
+  `cid` int UNSIGNED NOT NULL,
+  `aid` int UNSIGNED NOT NULL,
+  `nid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `depth` tinyint NOT NULL,
+  `content` json NOT NULL,
+  `reply_to` int UNSIGNED NOT NULL,
+  `children` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `type` varchar(16) NOT NULL,
+  `history` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rv_comments_p`
+--
+
+CREATE TABLE `rv_comments_p` (
+  `cid` int UNSIGNED NOT NULL,
+  `pid` int UNSIGNED NOT NULL,
+  `nid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `depth` tinyint UNSIGNED NOT NULL,
+  `content` json NOT NULL,
+  `reply_to` int UNSIGNED NOT NULL,
+  `children` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `type` tinyint NOT NULL,
+  `history` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rv_comments_u`
+--
+
+CREATE TABLE `rv_comments_u` (
+  `cid` int UNSIGNED NOT NULL,
+  `to_uid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `reply_to` int UNSIGNED NOT NULL,
+  `content` json NOT NULL,
+  `type` tinyint NOT NULL,
+  `history` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rv_posts`
+--
+
+CREATE TABLE `rv_posts` (
+  `pid` int UNSIGNED NOT NULL,
+  `aid` int UNSIGNED NOT NULL,
+  `fid` int UNSIGNED NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `title` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `is_conversation` tinyint(1) NOT NULL DEFAULT '0',
+  `viewer` json DEFAULT NULL,
+  `history` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -300,14 +421,7 @@ CREATE TABLE `roles` (
 CREATE TABLE `statistics` (
   `name` varchar(64) NOT NULL,
   `value` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `statistics`
---
-
-INSERT INTO `statistics` (`name`, `value`) VALUES
-('test', '{\"a\": 1}');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -318,7 +432,7 @@ INSERT INTO `statistics` (`name`, `value`) VALUES
 CREATE TABLE `tags` (
   `name` varchar(64) NOT NULL,
   `statistics` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -327,17 +441,17 @@ CREATE TABLE `tags` (
 --
 
 CREATE TABLE `users` (
-  `uid` int(10) UNSIGNED NOT NULL,
-  `username` varchar(256) CHARACTER SET ascii NOT NULL,
+  `uid` int UNSIGNED NOT NULL,
+  `username` varchar(256) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   `nickname` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `password` varchar(88) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `email` varchar(256) CHARACTER SET ascii NOT NULL,
+  `email` varchar(256) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   `avatar` varchar(256) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
   `about` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `statistics` json NOT NULL,
   `roles` longtext NOT NULL,
   `preferences` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
@@ -364,7 +478,8 @@ ALTER TABLE `articles`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`cid`),
-  ADD KEY `name` (`name`);
+  ADD KEY `name` (`name`),
+  ADD KEY `type` (`type`);
 
 --
 -- Indexes for table `comments_a`
@@ -393,7 +508,7 @@ ALTER TABLE `comments_u`
 -- Indexes for table `config`
 --
 ALTER TABLE `config`
-  ADD PRIMARY KEY (`flag`) USING HASH;
+  ADD PRIMARY KEY (`flag`);
 
 --
 -- Indexes for table `favorite_set`
@@ -446,10 +561,44 @@ ALTER TABLE `reports`
   ADD KEY `tid` (`tid`);
 
 --
--- Indexes for table `roles`
+-- Indexes for table `rv_articles`
 --
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`name`);
+ALTER TABLE `rv_articles`
+  ADD PRIMARY KEY (`aid`),
+  ADD UNIQUE KEY `slug` (`slug`),
+  ADD KEY `category` (`category`),
+  ADD KEY `uid` (`uid`);
+
+--
+-- Indexes for table `rv_comments_a`
+--
+ALTER TABLE `rv_comments_a`
+  ADD PRIMARY KEY (`cid`),
+  ADD KEY `aid` (`aid`),
+  ADD KEY `nid` (`nid`);
+
+--
+-- Indexes for table `rv_comments_p`
+--
+ALTER TABLE `rv_comments_p`
+  ADD PRIMARY KEY (`cid`),
+  ADD KEY `aid` (`pid`),
+  ADD KEY `nid` (`nid`);
+
+--
+-- Indexes for table `rv_comments_u`
+--
+ALTER TABLE `rv_comments_u`
+  ADD PRIMARY KEY (`cid`),
+  ADD KEY `aid` (`to_uid`);
+
+--
+-- Indexes for table `rv_posts`
+--
+ALTER TABLE `rv_posts`
+  ADD PRIMARY KEY (`pid`),
+  ADD KEY `aid` (`aid`),
+  ADD KEY `fid` (`fid`);
 
 --
 -- Indexes for table `statistics`
@@ -479,73 +628,103 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `aid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `aid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comments_a`
 --
 ALTER TABLE `comments_a`
-  MODIFY `cid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comments_p`
 --
 ALTER TABLE `comments_p`
-  MODIFY `cid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comments_u`
 --
 ALTER TABLE `comments_u`
-  MODIFY `cid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `favorite_set`
 --
 ALTER TABLE `favorite_set`
-  MODIFY `fid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `fid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `forum`
 --
 ALTER TABLE `forum`
-  MODIFY `fid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `fid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `lid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `lid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `nid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `nid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `pid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `rid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `rid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rv_articles`
+--
+ALTER TABLE `rv_articles`
+  MODIFY `aid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rv_comments_a`
+--
+ALTER TABLE `rv_comments_a`
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rv_comments_p`
+--
+ALTER TABLE `rv_comments_p`
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rv_comments_u`
+--
+ALTER TABLE `rv_comments_u`
+  MODIFY `cid` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rv_posts`
+--
+ALTER TABLE `rv_posts`
+  MODIFY `pid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
